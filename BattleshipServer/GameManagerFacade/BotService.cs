ce11d1e02f9e6df0062c6b0a2f5a40bd648843e7
Evita <evita.sriupsaite@gmail.com>
2025-10-25 14:20:47 +0300

    gamemanager facade

---
+++ BattleshipServer/GameManagerFacade/BotService.cs
@@ -0,0 +1,49 @@
+﻿using BattleshipServer.Builders;
+using BattleshipServer.Data;
+using BattleshipServer.Domain;
+using BattleshipServer.Models;
+using BattleshipServer.Npc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipServer.GameManagerFacade
+{
+    public class BotService
+    {
+        public async void CreateBotGame(GameManager manager, PlayerConnection player, bool isStandart, Database db, List<ShipDto> ships)
+        {
+            // 1) Bot žaidėjas su NoopWebSocket
+            var botSocket = new NoopWebSocket();
+            var bot = new PlayerConnection(botSocket, manager) { Name = "Robot" };
+
+            // 2) Pasirenkam konkretų builder'į
+            IGameSetupBuilder builder = isStandart
+                ? new StandardGameBuilder()
+                : new MiniGameBuilder();
+
+            // 3) „surenkam“ žaidimą (fluent seka)
+            var game = builder
+                .CreateShell(player, bot, manager, db)
+                .ConfigureBoard()
+                .ConfigureFleets(ships, opponentRandom: true)
+                .ConfigureNpc(g =>
+                {
+                    var selector = new RuleBasedSelector();
+                    return new BotOrchestrator(g, bot.Id, selector, "checkerboard");
+                })
+                .Build();
+
+            // 4) Registracija ir BotOrchestrator užkabinimas GameManager'io žemėlapyje
+            var orch = builder.Orchestrator!;
+            manager.AddGame(game, player.Id, orch);
+            
+            // 5) Start
+            await game.StartGame();
+
+            Console.WriteLine($"[Manager] Player {player.Name} started BOT game (builder).");
+        }
+    }
+}
---
+++ BattleshipServer/GameManagerFacade/MessageDtoService.cs
@@ -0,0 +1,49 @@
+﻿using BattleshipServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BattleshipServer.GameManagerFacade
+{
+    public class MessageDtoService
+    {
+        public List<ShipDto> GetShipsFromDto(MessageDto dto)
+        {
+            var ships = new List<ShipDto>();
+            if (dto.Payload.TryGetProperty("ships", out var shEl))
+            {
+                foreach (var el in shEl.EnumerateArray())
+                {
+                    ships.Add(new ShipDto
+                    {
+                        X = el.GetProperty("x").GetInt32(),
+                        Y = el.GetProperty("y").GetInt32(),
+                        Len = el.GetProperty("len").GetInt32(),
+                        Dir = el.GetProperty("dir").GetString()
+                    });
+                }
+            }
+            return ships;
+        }
+
+        private bool GetBool(MessageDto dto, string attrName, bool defaultValue)
+        {
+            if (dto.Payload.TryGetProperty(attrName, out var val) && (val.ValueKind == JsonValueKind.True || val.ValueKind == JsonValueKind.False))
+                return val.GetBoolean();
+            return defaultValue;
+        }
+
+        public bool GetIsStandartGame(MessageDto dto)
+        {
+            return GetBool(dto, "isStandartGame", true);
+        }
+
+        public bool GetIsDoubleBomb(MessageDto dto)
+        {
+            return GetBool(dto, "doubleBomb", false);
+        }
+    }
+}
